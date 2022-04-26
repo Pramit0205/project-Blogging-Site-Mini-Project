290b14b3f0a485bd24abb9bb09dd62da6d897792
Pratik <you@example.com>
2022-04-27 00:38:51 +0530

    origin/blogging_project

---
+++ src/Model/autherModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+//{ fname: { mandatory}, lname: {mandatory},
+// title: {mandatory, enum[Mr, Mrs, Miss]}, email: {mandatory, valid email, unique}, password: {mandatory} }
+
+const autherSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    title:{ type:String, required: true , enum :["Mr","Mrs","Miss"] } ,
+    email:{ type:String, required: true , unique:true },
+    password:{ type:String , required:true}
+
+}, { timestamps: true });
+
+ 
+module.exports= mongoose.model('auther' , autherSchema )
